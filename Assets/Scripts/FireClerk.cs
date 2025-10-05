346ccb40e308169d59fc12de206d9efb7eed167b
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-10-05 17:31:45 +0200

    Clerk on fire

---
+++ Assets/Scripts/FireClerk.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+[CreateAssetMenu (menuName = "Mischief Events/ Fire Clerk")]
+public class FireClerk : MischiefEvent
+{
+    private void OnEnable()
+    {
+        OnMischief.AddListener(Mischief);
+    }
+
+    private void OnDisable()
+    {
+        OnMischief.RemoveListener(Mischief);
+    }
+    
+    private void Mischief()
+    {
+        Debug.Log("clerk on fire");
+        allowMischief = false;
+        if (!interactableObject.GetComponent<Animator>()) return;
+        Animator animator = interactableObject.GetComponent<Animator>();
+        interactableObject.GetComponent<Animator>().SetTrigger("Start");
+        VisualEffect ve = interactableObject.GetComponentInChildren<VisualEffect>();
+        if (ve is null) return;
+        ve.enabled = true;
+    }
+}
